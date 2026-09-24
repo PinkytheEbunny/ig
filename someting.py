ec71bb414a8cd90f935f4844301f412ea4dfeb0a
PinkytheEbunny <JTaylor080910@gmail.com>
2026-09-24 19:34:57 +0000

    FIXED

--- someting.py
+++ someting.py
@@ -3,31 +3,47 @@ import random
 import requests
 
 print("Welcome To The Pokedex!")
-while True:  
-    choices = input("What would you like to do today?\n 1.) Choose A Random Pokemon \n 2.) Look Up A Pokemon \n 3.) Exit:\n")
+# Get ONLY Pokemon names
+pokemon_url = "https://pokeapi.co/api/v2/pokemon?limit=1025"
+all_pokemon = requests.get(pokemon_url).json()["results"]
+# Make a list of just the names
+pokemon_names = [pokemon["name"] for pokemon in all_pokemon]
+while True:
+    choices = input(
+        "What would you like to do today?\n"
+        " 1.) Choose A Random Pokemon\n"
+        " 2.) Look Up A Pokemon\n"
+        " 3.) Exit:\n"
+    )
     if choices == "1":
         random_id = random.randint(1, 1025)
-        random_url = f'https://pokeapi.co/api/v2/pokemon/{random_id}'
+        random_url = f"https://pokeapi.co/api/v2/pokemon/{random_id}"
         data2 = requests.get(random_url).json()
         pokemon_png = data2["sprites"]["front_default"]
-        random_name = data2['name'].capitalize()
-        sprite_url = data2['sprites']['front_default']
         print(f"Name: {data2['name'].capitalize()}")
         print(f"ID: {data2['id']}")
         print(f"Height: {data2['height']}")
         print(f"Weight: {data2['weight']}")
         print(f"PNG Sprite URL: {pokemon_png}")
-
     elif choices == "2":
-
-        try:
-            look_up_a_pokemon = input("What pokemon would you like to look up?\n").lower()
-            picked_pokemon = f'https://pokeapi.co/api/v2/pokemon/{look_up_a_pokemon}' or f'https://pokeapi.co/api/v2/pokemon/{new_input}'
-            data3 = requests.get(url).json()["results"]
+        look_up_a_pokemon = input(
+            "What pokemon would you like to look up?\n"
+        ).lower().strip()
+        # Check if the name is a real Pokemon
+        if look_up_a_pokemon in pokemon_names:
+            picked_pokemon = (
+                f"https://pokeapi.co/api/v2/pokemon/{look_up_a_pokemon}"
+            )
+            data3 = requests.get(picked_pokemon).json()
             picked_pokemon_png = data3["sprites"]["front_default"]
-            picked_name = data3['name'].capitalize()
-            sprite_url = data3['sprites']['front_default']
-            what_u_wanna_know = input(f"What would you like to know about {look_up_a_pokemon}?\n 1.) Its weight? \n 2.) Its height? \n 3.) Its Id? \n 4.) All of The Above\n")
+            what_u_wanna_know = input(
+                f"What would you like to know about "
+                f"{look_up_a_pokemon.capitalize()}?\n"
+                " 1.) Its weight?\n"
+                " 2.) Its height?\n"
+                " 3.) Its Id?\n"
+                " 4.) All of The Above\n"
+            )
             if what_u_wanna_know == "1":
                 print(f"Weight: {data3['weight']}")
                 print(f"PNG Sprite URL: {picked_pokemon_png}")
@@ -44,60 +60,17 @@ while True:
                 print(f"PNG Sprite URL: {picked_pokemon_png}")
             else:
                 print("That is not a valid choice.")
-        except:
-                url = "https://pokeapi.co/api/v2/pokemon?limit=2000"
-                all_pokemon = requests.get(url).json()["results"]
-                print("did you mean any of these?")
-                for poke in all_pokemon:
-                    if poke["name"].startswith(look_up_a_pokemon):
-                        print(poke["name"].capitalize())
-                new_input = input("If it is please type that here:\n").lower()
+        else:
+            print("\nDid you mean any of these?")
+            found_pokemon = False
+            for pokemon in pokemon_names:
+                if pokemon.startswith(look_up_a_pokemon):
+                    print(pokemon.capitalize())
+                    found_pokemon = True
+            if not found_pokemon:
+                print("No Pokemon found starting with that.")
+            print("Please put one of the listed Pokemon names in the search.")
     elif choices == "3":
         sys.exit(0)
     else:
         print("Sorry! That is not a choice!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# list_of_people = data['people']
-# total = 0
-# for person in list_of_people:
-#     total += 1 
-
-# print(total)
-# for person in data['people']:
-#     if person['craft'] not in all_crafts:
-#         all_crafts.append(person['craft'])
-
-# print("Many people are currently in space")
-# print("All those in space are either on one of the following ships:")
-# counter = 1
-# for craft in all_crafts:
-#     print(f"{counter} - {craft}")
-#     counter += 1
-
-# user_choice = input("Which spacecraft are you interested in? (choose a number):")
-# craft_chosen = all_crafts[int(user_choice) - 1]
-
-# print(f"the current crew of the {craft_chosen} includes:")
-# for person in data['people']:
-#     if person['craft'] == craft_chosen:
-#         print(f" - {person['name']}")
